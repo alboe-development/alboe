b746a1551755d08743ae29c10e8a2923c176a062
Timothy Scheuering <InteractiveTimmy@users.noreply.github.com>
2023-06-27 21:51:18 -0700

    feat(config/eslint): generate package (#4)

---
+++ packages/config/eslint/static/typescript/typescript.js
@@ -0,0 +1,47 @@
+const config = {
+  overrides: [
+    {
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+      ],
+      files: [
+        '*.ts',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: [
+        'eslint-plugin-jsdoc',
+        'eslint-plugin-tsdoc',
+      ],
+      rules: {
+        'import/extensions': 'off',
+        'import/no-relative-packages': 'off',
+        'import/prefer-default-export': 'off',
+        'jsdoc/require-jsdoc': 'error',
+        'max-len': [
+          'error', {
+            code: 120,
+          },
+        ],
+        'tsdoc/syntax': 'error',
+      },
+      settings: {
+        'import/extensions': [
+          '.ts',
+        ],
+        'import/resolver': {
+          node: {
+            extensions: [
+              '.ts',
+            ],
+          },
+        },
+      },
+    },
+  ],
+};
+
+module.exports = config;
