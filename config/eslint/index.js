17ed367e34b293a780d2ce778257203ca6851a1e
Timothy Scheuering <interactivetimmy@gmail.com>
2024-09-22 15:35:09 -0700

    refactor(config): remove legacy config

--- config/eslint/index.js
+++
@@ -1,9 +0,0 @@
-const javascript = require('./javascript');
-const jest = require('./jest');
-const typescript = require('./typescript');
-
-module.exports = {
-  javascript,
-  jest,
-  typescript,
-};
--- config/jest/index.js
+++
@@ -1,5 +0,0 @@
-const base = require('./jest.base');
-
-module.exports = {
-  base,
-};
