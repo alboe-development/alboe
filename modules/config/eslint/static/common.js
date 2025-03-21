2c741949102fb8eaa3c8d95dc89c342fb05a95fe
InteractiveTimmy <interactivetimmy@gmail.com>
2025-03-21 14:29:03 -0700

    feat(config/eslint): add tsdoc and jsdoc

--- modules/config/eslint/static/common.js
+++ modules/config/eslint/static/common.js
@@ -1,10 +1,17 @@
-const rules = {
-  'eol-last': ['error', 'always'],
-  'max-len': ['error', { code: 120, comments: 100 }],
-  quotes: ['error', 'single'],
-  semi: ['error', 'always'],
-};
+/**
+ * Generate ESLint Common configuration for a module within this project.
+ *
+ * @returns {Array} - The generated configuration.
+ */
+const generate = () => ([
+  {
+    rules: {
+      'eol-last': ['error', 'always'],
+      'max-len': ['error', { code: 120, comments: 100 }],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
+]);
 
-module.exports = {
-  rules,
-};
+module.exports = generate;
--- modules/config/eslint/static/javascript.js
+++ modules/config/eslint/static/javascript.js
@@ -1,28 +1,71 @@
-const js = require('@eslint/js');
+const javascript = require('@eslint/js');
+const jsdoc = require('eslint-plugin-jsdoc');
 
+/**
+ * @typedef Definition
+ * @type {object}
+ * @property {string} [type] - The module definition type to generate for.
+ */
+
+/**
+ * @typedef Options
+ * @type {object}
+ * @property {Definition} [definition] - The module definition to generate for.
+ */
+
+/**
+ * Generate ESLint JavaScript configuration for a module within this project.
+ *
+ * @param {Options} [options] - Options to use when generating the configuration.
+ * @returns {Array} - The generated configuration.
+ */
 const generate = (options = {}) => {
   const { definition = {} } = options;
   const type = definition.type ? definition.type.toLowerCase() : 'commonjs';
-  const files = [];
+  const languageOptions = { sourceType: type };
+  const generalFiles = type === 'module' ? ['**/*.{js,cjs}'] : ['**/*.{js,mjs}'];
+  const sourceFiles = type === 'module' ? ['{src,static}/**/*.{js,cjs}'] : ['{src,static}/**/*.{js,mjs}'];
 
-  switch (type) {
-    case 'commonjs':
-      files.push('**/*.js', '**/*.mjs');
-      break;
+  const general = [
+    javascript.configs.recommended,
+  ].map((config) => ({
+    ...config,
+    files: generalFiles,
+    languageOptions,
+  }));
 
-    case 'module':
-      files.push('**/*.js', '**/*.cjs');
-      break;
-  }
-
-  return [
+  const source = [
+    jsdoc.configs['flat/recommended'],
     {
-      ...js.configs.recommended,
-      files,
-      languageOptions: {
-        sourceType: type,
+      plugins: {
+        jsdoc,
+      },
+      rules: {
+        'jsdoc/require-jsdoc': [
+          'warn',
+          {
+            publicOnly: { ancestorsOnly: false },
+            require: {
+              FunctionDeclaration: true,
+              FunctionExpression: true,
+              ArrowFunctionExpression: true,
+              ClassDeclaration: true,
+              ClassExpression: true,
+              MethodDefinition: true,
+            },
+          },
+        ],
+        'jsdoc/tag-lines': 'off',
       },
     },
+  ].map((config) => ({
+    ...config,
+    files: sourceFiles,
+  }));
+
+  return [
+    ...general,
+    ...source,
   ];
 };
 
