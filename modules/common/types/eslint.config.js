ffa8b3723198e1909520c579e600f9114ecea71d
InteractiveTimmy <interactivetimmy@gmail.com>
2025-05-04 17:58:34 -0700

    feat(common/types): create module

---
+++ modules/common/types/eslint.config.js
@@ -0,0 +1,10 @@
+const { common, javascript, typescript } = require('@alboe/eslint-config');
+const definition = require('./package.json');
+
+const config = [
+  ...javascript({ definition }),
+  ...typescript({ definition }),
+  ...common(),
+];
+
+module.exports = config;
